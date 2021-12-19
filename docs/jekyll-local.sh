42ae607693f908a68f5ddacc27d91ab9f80fa7cd
Maave <1850819+maave0@users.noreply.github.com>
2021-12-18 20:56:43 -0500

    docs, gitignore for docs

--- docs/jekyll-local.sh
+++ docs/jekyll-local.sh
@@ -1,6 +1,5 @@
-docker run --rm \
-  --volume="$PWD:/srv/jekyll" \
-  --publish [::1]:8080:8080 \
-  jekyll/jekyll:pages \
-  jekyll serve \
-  #--watch --drafts
+docker run \
+  -t --rm \
+  -v "$PWD":/usr/src/app \
+  -e JEKYLL_GITHUB_TOKEN=$JEKYLL_GITHUB_TOKEN \
+  -p "4000:4000" starefossen/github-pages
